25005e90b09f0f3053a57d40792622cd9e2fa2bf
Zafar00 <mdzafarddd@gmail.com>
2025-06-27 13:34:00 +0530

    improvements

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about/me')
+def about():
+    return render_template('about.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('education.html')
+
+@app.route('/cntct')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/work')
+def work():
+    return render_template('project.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
